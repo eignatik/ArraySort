ef2d8c08cf3c8cd4015f7e6c22fdb7df08f745d3
eignatik <light.ji@yandex.ru>
2016-10-22 21:00:49 -0400

    Refactoring: changed method of changing values in elements because it didn't work with 0 and negative values, added print like array and string, added StringBuilder in print and getting array as string

--- src/ArraySortings.java
+++ src/ArraySortings.java
@@ -1,10 +1,6 @@
 import java.util.Random;
 
 public class ArraySortings {
-    public ArraySortings(){
-
-    }
-
     public int[] fillRandomArray(int[] array){
         Random random = new Random();
         for(int i=0; i<array.length; i++){
@@ -14,21 +10,36 @@ public class ArraySortings {
     }
 
     public void printArray(int[] array){
+        StringBuilder arrayBuilder = new StringBuilder();
         for(int i=0; i<array.length; i++){
-            System.out.println(array[i] +  " ");
+            arrayBuilder.append(array[i]).append(" ");
         }
+        System.out.println(arrayBuilder);
+    }
+
+    public void printArray(String array){
+        System.out.println(array);
     }
 
     public int[] sortEvenNumbers(int[] array){
         int indexOfLastEven = 0;
+        int localElement;
         for(int i=0; i<array.length; i++){
             if(array[i] % 2 == 0 && array[i] != 0) {
-                array[indexOfLastEven] = array[indexOfLastEven] + array[i];
-                array[i] = array[indexOfLastEven] - array[i];
-                array[indexOfLastEven] = array[indexOfLastEven] - array[i];
+                localElement = array[indexOfLastEven];
+                array[indexOfLastEven] = array[i];
+                array[i] = localElement;
                 indexOfLastEven ++;
             }
         }
         return array;
+    }
+
+    public String getArrayAsString(int[] array){
+        StringBuilder arrayBuilder = new StringBuilder();
+        for(int i=0; i<array.length; i++){
+            arrayBuilder.append(array[i]).append(" ");
+        }
+        return arrayBuilder.toString();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int[] array = {0, 0, 2, 4, 6, 7, 8, 9};
+        int[] array = new int[5];
         ArraySortings sortings = new ArraySortings();
-        //array = sortings.fillRandomArray(array);
+        array = sortings.fillRandomArray(array);
         sortings.printArray(array);
         array = sortings.sortEvenNumbers(array);
         System.out.println("****\n\n");
